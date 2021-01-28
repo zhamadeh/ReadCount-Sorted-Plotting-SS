982094afadf76c23fc2a0ae80bf027897e3a3d20
zhamadeh <zeid.hamadeh@hotmail.com>
2021-01-27 16:35:39 -0800

    almost forgot, se

--- R/breakpointr.R
+++ R/breakpointr.R
@@ -264,6 +264,6 @@ if (printer=="feature"){
     }
 }
 
-breakpointr(inputfolder=args[1],outputfolder = args[2], printer=args[3],feature=args[4],numLibsToShow = args[5],halfHalf=args[6], pairedEndReads = T,numCPU = 13,windowsize=175,binMethod="reads",peakTh=0.3875,min.mapq=7.75,trim=6.5,background=0.15,multi.sizes=NULL,genoT = "fisher")
+breakpointr(inputfolder=args[1],outputfolder = args[2], printer=args[3],feature=args[4],numLibsToShow = args[5],halfHalf=args[6], pairedEndReads = F,numCPU = 13,windowsize=175,binMethod="reads",peakTh=0.3875,min.mapq=7.75,trim=6.5,background=0.15,multi.sizes=NULL,genoT = "fisher")
 
 
